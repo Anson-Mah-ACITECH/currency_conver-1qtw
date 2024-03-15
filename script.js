200bc8af13f568a86b836375bf965dff6a20fc3e
Anson-Mah-ACITECH <mah9689@student.acitech.org>
2024-03-15 09:13:21 -0400

    Two Changes: Hid the overflow & added exchange rates

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Name: Anson Mah
 // Date: 14 March, 2024 (π Day)
+
+const exchange_rates = {
+  "AUD": {
+    'AUD': 1.00,
+    'FJD': 1.47, 
+    'NZD': 1.08, 
+    'PGK': 2.52, 
+    'TOP': 1.55, 
+    'USD': 0.66, 
+    'VUV': 78.45, 
+    'WST': 1.80, 
+    'XPF': 72.04, 
+  }
+}
